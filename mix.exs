0db44d0c8a5bb16da373b655aa11294ccc3cd3de
Alan Vardy <alan@vardy.cc>
2023-09-16 06:42:00 -0700

    update riakc

--- mix.exs
+++ mix.exs
@@ -2,27 +2,30 @@ defmodule Riak.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :riak,
+    [
+      app: :riak,
       version: "1.1.6",
       build_per_environment: false,
       name: "Riak Elixir Client",
       source_url: "https://github.com/drewkerrigan/riak-elixir-client",
       deps: deps(),
       description: description(),
-      package: package() ]
+      package: package()
+    ]
   end
 
   # Configuration for the OTP application
   def application do
-    [ applications: [ :pooler ],
-      included_applications: [ :riakc ] ]
+    [applications: [:pooler], included_applications: [:riakc]]
   end
 
   defp deps do
-    [ {:ex_doc, ">= 0.0.0", only: :dev},
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev},
       {:pooler, "~> 1.5.3"},
-      {:riakc, "~> 2.5.3"} ]
+      {:riakc, github: "basho/riak-erlang-client", tag: "3.0.13"}
+    ]
   end
 
   defp description do
@@ -32,10 +35,14 @@ defmodule Riak.Mixfile do
   end
 
   defp package do
-    [ files: ~w(config doc examples lib test LICENSE mix.exs mix.lock README.md THANKS),
-      maintainers: [ "Drew Kerrigan", "Eduardo Gurgel", "Randy Secrist" ],
-      licenses: [ "Apache 2.0" ],
-      links: %{"GitHub" => "https://github.com/drewkerrigan/riak-elixir-client",
-               "Documentation" => "http://hexdocs.pm/riak"} ]
+    [
+      files: ~w(config doc examples lib test LICENSE mix.exs mix.lock README.md THANKS),
+      maintainers: ["Drew Kerrigan", "Eduardo Gurgel", "Randy Secrist"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/drewkerrigan/riak-elixir-client",
+        "Documentation" => "http://hexdocs.pm/riak"
+      }
+    ]
   end
 end
